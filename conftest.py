f943f9af04677f53567448fc0008cfef4083bb15
Roman Kolchin <[breezy10mu@gmail.com]>
2020-06-01 12:49:00 +0300

    some changes

--- conftest.py
+++ conftest.py
@@ -4,8 +4,8 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default='en',
-                     help="Choose language: ru, en, ... (etc.)")
+    parser.addoption('--language', action='store', default="en")
+                     
 
 
 @pytest.fixture(scope="function")
